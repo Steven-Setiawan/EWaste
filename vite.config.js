3450db2bf8848ab7adb0fa36ee857a7e62ab4dac
Steven Jimmy Setiawan <stevenjimmysetiawan@outlook.com>
2024-12-10 14:03:27 +0700

    FINALLLLL

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            'process.env.VITE_APP_NAME': process.env.VITE_APP_NAME,
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
